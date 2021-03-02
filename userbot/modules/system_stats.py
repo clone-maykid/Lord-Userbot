2d566bf0c69cb7692cca264e8649da1004634481
clone-maykid <79383226+clone-maykid@users.noreply.github.com>
2021-03-03 03:05:58 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -223,12 +223,12 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f" **┗┓LORD USERBOT┏┛** \n"
-        f"\n__**{LORD_TEKS_KUSTOM}**__\n"
+        f" **┗┓MAYKID USERBOT┏┛** \n"
+        f"\n__**{MAYKID_TEKS_KUSTOM}**__\n"
         f"**━━━━━━━━━━━━━━━━━━━━**\n"
-        f"**♛ Lord** \n"
+        f"**⚡️ MAYKID** \n"
         f" ➥ `{DEFAULTUSER}` \n"
-        f"**♛ Username** \n"
+        f"**⚡️ Username** \n"
         f" ➥ `@{user.username}` \n"
         f"┏━━━━━━━━━━━━━━━━━━━\n"
         f"┣[• `Telethon :`Ver {version.__version__} \n"
@@ -261,16 +261,16 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f"**▬▬▬▬▬▬❙۩♛۩❙▬▬▬▬▬▬**\n"
-        f"     **♕ LORD USERBOT ♕** \n\n"
-        f"❃ **Lord**     \n   ➥ `{DEFAULTUSER}` \n"
-        f"❃ **Username** \n   ➥ `@{user.username}` \n"
-        f"❃ **Telethon** \n   ➥ `Versi {version.__version__}` \n"
-        f"❃ **Python**   \n   ➥ `Versi {python_version()}` \n"
-        f"❃ **Versi Bot**\n   ➥ `{BOT_VER}` \n"
-        f"❃ **Modul**    \n   ➥ `{len(modules)}` \n\n"
-        f"❃ **Repo Userbot:** [Lord-Userbot](https://github.com/Zora24/Lord-Userbot)\n❃ **Grup Userbot: **[Tekan](t.me/LordUserbot_Group)\n❃ **Pemilik:** [Alvin](t.me/liualvinas)\n"
-        f"**▬▬▬▬▬▬❙۩★۩❙▬▬▬▬▬▬**")
+        f"**▬▬▬▬▬▬❙۩⚡️۩❙▬▬▬▬▬▬**\n"
+        f"     **⚡️ MAYKID USERBOT ⚡️** \n\n"
+        f"⚡️ **Maykid**     \n   ➥ `{DEFAULTUSER}` \n"
+        f"⚡️ **Username** \n   ➥ `@{user.username}` \n"
+        f"⚡️ **Telethon** \n   ➥ `Versi {version.__version__}` \n"
+        f"⚡️ **Python**   \n   ➥ `Versi {python_version()}` \n"
+        f"⚡️ **Versi Bot**\n   ➥ `{BOT_VER}` \n"
+        f"⚡️ **Modul**    \n   ➥ `{len(modules)}` \n\n"
+        f"⚡️ **Repo Userbot:** [Maykid-Userbot](https://github.com/clone-maykid/Lord-Userbot)\n⚡️ **Grup Userbot: **[Tekan](t.me/LordUserbot_Group)\n⚡️ **Pemilik:** [gunggg](t.me/aestheticboyy2)\n"
+        f"**▬▬▬▬▬▬❙۩⚡️۩❙▬▬▬▬▬▬**")
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
@@ -296,16 +296,16 @@ async def amireallyalive(alive):
     user = await bot.get_me()
     await get_readable_time((time.time() - StartTime))
     output = (
-        f" **┗┓LORD USERBOT┏┛** \n\n"
-        f"♛ **Lord:** \n"
+        f" **┗┓MAYKID USERBOT┏┛** \n\n"
+        f"⚡️ **Maykid:** \n"
         f" `{DEFAULTUSER}` \n"
-        f"♛ **Username:** \n"
+        f"⚡️ **Username:** \n"
         f" `@{user.username}` \n\n"
-        f"__Lord-Userbot Adalah Userbot Khusus__ "
+        f"__Maykid-Userbot Adalah Userbot Khusus__ "
         f"__Yang Digunakan Untuk Bersenang-Senang__ "
         f"__Di Telegram__ __Versi Bot Yang Digunakan__ `{BOT_VER}` "
-        f"__Jumlah Modul Dalam Lord Userbot__ `{len(modules)}` \n\n"
-        f"❃ **Repo Userbot:** [Lord-Userbot](https://github.com/Zora24/Lord-Userbot)\n❃ **Grup Userbot: **[Tekan](t.me/LordUserbot_Group)\n❃ **Pemilik:** [Alvin](t.me/liualvinas)\n")
+        f"__Jumlah Modul Dalam Maykid Userbot__ `{len(modules)}` \n\n"
+        f"⚡️ **Repo Userbot:** [Maykid-Userbot](https://github.com/clone-maykid/Lord-Userbot)\n⚡️ **Grup Userbot: **[Tekan](t.me/LordUserbot_Group)\n⚡️ **Pemilik:** [gunggg](t.me/aestheticboyy2)\n")
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
